3a0e1fe8d4b88a10b9895e6b76418b1ca0c5c014
LuukvanDooremalen <90319668+LuukvanDooremalen@users.noreply.github.com>
2023-11-28 11:38:04 +0100

    Jaar blokken toegevoegd

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 particlesJS.load('particles-js', 'particles-config.json');
 
-// moving emoji
-const element = document.querySelector('.delay-element');
-let mouseX = 0;
-let mouseY = 0;
 
-document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-});
+// const jaar1 = document.getElementById('jaar1');
 
-function updateElementPosition() {
-    const currentX = parseFloat(element.style.left) || 0;
-    const currentY = parseFloat(element.style.top) || 0;
+// const headerslide = document.getElementById('cool-animation');
 
-    const deltaX = mouseX - currentX;
-    const deltaY = mouseY - currentY;
+const jaarElements = document.getElementsByClassName('jaar');
 
-    // Adjust these values to control the speed of following
-    const speed = 0.08;
-
-    element.style.left = currentX + speed * deltaX + 'px';
-    element.style.top = currentY + speed * deltaY + 'px';
-
-    requestAnimationFrame(updateElementPosition);
+// Assuming you want to add the click event to all elements with the class 'jaar'
+for (let i = 0; i < jaarElements.length; i++) {
+    jaarElements[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
 }
 
-updateElementPosition();
+// moving emoji
+// const element = document.querySelector('.delay-element');
+// let mouseX = 0;
+// let mouseY = 0;
+
+// document.addEventListener('mousemove', (e) => {
+//     mouseX = e.clientX;
+//     mouseY = e.clientY;
+// });
+
+// function updateElementPosition() {
+//     const currentX = parseFloat(element.style.left) || 0;
+//     const currentY = parseFloat(element.style.top) || 0;
+
+//     const deltaX = mouseX - currentX;
+//     const deltaY = mouseY - currentY;
+
+//     // Adjust these values to control the speed of following
+//     const speed = 0.08;
+
+//     element.style.left = currentX + speed * deltaX + 'px';
+//     element.style.top = currentY + speed * deltaY + 'px';
+
+//     requestAnimationFrame(updateElementPosition);
+// }
+
+// updateElementPosition();
+
+
+
+
 
 
